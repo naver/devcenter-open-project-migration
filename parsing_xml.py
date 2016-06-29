db514c58e25e60d4fcf1af4881d1e1261a966813
Taehwan Kim <maxtortime@navercorp.com>
2016-06-29 19:14:36 +0900

    Can move Issue and it's comment
    !! 코드에 똑같은 주석이 있음
    
    1. 이슈의 게시물과 댓글을 옮기는 것은 가능
    2. 게시판도 XML 구조가 유사할 것으로 추정되므로 똑같이 돌리면 돌아갈
    것으로 추정함
    
    ISSUE:
    1. 이슈를 쓴 사람과 이슈가 배정된 사람을 지정할 수가 없음
    2. API 구조상 댓글을 작성한 사람을 지정할 수가 없고 무조건 현재
    access_token 에 해당하는
    유저로 설정되는데 조사해봐야 할 것 같다.
    3. 첨부파일이나 댓글에 HTML 태그가 들어가는 건 어찌.. (Github 댓글은
    markdown이라..)

--- parsing_xml.py
+++ parsing_xml.py
@@ -1,6 +1,8 @@
 import xml.etree.ElementTree as ET
 import json
 import requests
+import os
+import time
 
 url = "https://api.github.com/repos/maxtortime/Hello-World/import/issues"
 
@@ -13,15 +15,33 @@ headers = {
     'User-Agent': 'postman-test'
 }
 
-tree = ET.parse('issue.xml')
-root = tree.getroot()
+for file_name in os.listdir('./boards/'):
+    tree = ET.parse(str('./boards/' + file_name))
+    root = tree.getroot()
 
-for child in root:
-    issue_json = json.dumps({"issue" : { "title" : child[4].text,
-                                         "body" : child[5].text,
-                                         "closed" : False if child[3].text is '0'
+    try:
+        comments = [
+            {
+                'date' : comment.find('pubDate').text if comment.find('pubDate') != None else '0',
+                'desc' : comment.find('description').text if comment.find('description') != None else 'None'
+            }
+        for comment in root[13].findall('item')]
+    except IndexError:
+        continue
+
+    issue_json = json.dumps({"issue" : { "title" : root[4].text,
+                                         "body" : root[5].text,
+                                         "closed" : False if root[3].text is '0'
                                          else True
-                                        }})
+                                        },
+                             "comments" : [
+                                {
+                                    "created_at" :
+                                    time.strftime("%Y-%m-%dT%H:%M:%SZ",
+                                    time.localtime(int(comment['date']))),
+                                    "body" : comment['desc']
+                                }
+                             for comment in comments]})
 
     response = requests.request("POST", url, data=issue_json, headers=headers)
     print(response.text)

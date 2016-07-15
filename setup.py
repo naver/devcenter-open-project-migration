481d04a848433b3565b4470400f3755edbef7c73
Taehwan Kim <maxtortime@navercorp.com>
2016-07-15 15:21:34 +0900

    Structuring for project

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup
 
+with open('README.rst') as f:
+    readme = f.read()
+
 setup(
     name='migration',
-    version='0.0.1',
+    version='0.1',
+    description='Naver open project migration'
+    long_description=readme,
+    author='Taehwan Kim',
+    author_email='maxtortime@navercorp.com',
     py_modules=['migration'],
     install_requires=[
         'Click',

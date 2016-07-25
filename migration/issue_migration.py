2c0d54efc05a88f6c637f1208e0ba63548933844
Taehwan Kim <maxtortime@navercorp.com>
2016-07-25 15:50:12 +0900

    마일스톤 옮기는 기능 구현 (불완전)

--- migration/issue_migration.py
+++ migration/issue_migration.py
@@ -68,11 +68,103 @@ def file_download(attach_path, url, file_name, artifact_id, file_id, **kwargs):
     return status_code
 
 
+def milestone_migration(project, gh, repo_name, token):
+    milestones = project.milestones
+    milestone_post_url = '{0}/repos/{1}/{2}/milestones?access_token={3}'.format(gh._github_url, gh.user().login,
+                                                                                repo_name, token)
+    for milestone in milestones:
+        requests.request("POST", milestone_post_url, data=str(milestone))
+
+
+def get_attach_links(attachments, download_path, api_url, github_username, repo_name, artifact_id, github_url):
+    attach_links = ''
+
+    for item in attachments:
+        body_file_link = item.find('link').get_text()
+        body_file_name = item.find('name').get_text()
+        body_file_id = item.find('id').get_text()
+
+        down_url = "{0}{1}".format(api_url, body_file_link)
+        file_download(download_path, down_url, body_file_name, artifact_id, body_file_id)
+
+        body_uploaded_link = 'https://{5}/{0}/{1}/wiki/attachFile/{2}/{3}/{4}'.format(
+            github_username,
+            repo_name,
+            artifact_id,
+            body_file_id,
+            body_file_name,
+            github_url)
+
+        attach_links += '* {0}\n\n\t![{0}]({1})\n\n'.format(body_file_name, body_uploaded_link)
+
+    return attach_links
+
+
+def get_comments(comments, api_url, attach_download_path, artifact_id, github_username, repo_name):
+    result = []
+    comment_list = comments.find_all('item')
+    attach_links = ''
+
+    for comment in comment_list:
+        attachments = comment.attachments
+
+        if comment.find('pubDate') is None \
+                or comment.find('description') is None \
+                or comment.find('author') is None \
+                or comment.find('id') is None:
+            continue
+
+        id_ = comment.find('id').get_text()
+        body = comment.find('description').get_text()
+        author = comment.find('author').get_text()
+        date = comment.find('pubDate').get_text()
+
+        print_date_type = "%Y/%m/%d %H:%M:%S"
+        github_time_type = "%Y-%m-%dT%H:%M:%SZ"
+
+        print_time = time.strftime(print_date_type, time.localtime(int(date)))
+        github_time = time.strftime(github_time_type, time.localtime(int(date)))
+
+        if attachments:
+            attach_links = '\n\n-----\n### Attachments\n'
+            items = attachments.find_all('item')
+
+            for item in items:
+                file_id = item.find('id').get_text()
+                file_link = item.find('link').get_text()
+                file_name = file_link.split('/')[-1]
+                down_url = "{0}{1}".format(api_url, file_link)
+
+                file_download(attach_download_path, down_url, file_name, artifact_id, file_id, comment_id=id_)
+
+                uploaded_link = 'https://github.com/{0}/{1}/wiki/attachFile/{2}/{3}/{4}/{5}'.format(
+                    github_username,
+                    repo_name,
+                    artifact_id,
+                    id_,
+                    file_id,
+                    file_name)
+                attach_links += '* {0}\n\n\t![{0}]({1})\n\n'.format(file_name, uploaded_link)
+
+        c_body = "This comment created by **{0}** | {1}\n\n------\n\n{2}{3}".format(author, print_time, body,
+                                                                                    attach_links)
+
+        result.append({
+            "created_at": github_time,
+            "body": c_body
+        })
+
+    return result
+
+
 def issue_migration(**kwargs):
     click.echo('이슈 마이그레이션 중...')
+
+    # 기본 변수 정의
     project = kwargs.get('project')
     gh = kwargs.get('github_session')
     repo = kwargs.get('github_repository')
+    github_api_url = gh._github_url
 
     with open(BASIC_TOKEN_FILE_NAME) as f:
         token = f.read()
@@ -86,13 +178,21 @@ def issue_migration(**kwargs):
     project_name = str(project)
     github_username = gh.user().login
 
-    import_request_url = '{0}/repos/{1}/{2}/import/issues'.format(gh._github_url,
+    import_request_url = '{0}/repos/{1}/{2}/import/issues'.format(github_api_url,
                                                                   github_username,
                                                                   repo.name)
 
     issue_urls = project.urls.copy()
     del issue_urls['download']
 
+    github_parse_url = urlparse(github_api_url)
+    github_url = 'github.com' if github_parse_url.netloc == 'api.github.com' \
+        else github_parse_url.netloc
+
+    # 마이그레이션 마이그레이션
+    milestone_migration(project, gh, repo.name, token)
+
+    # 이슈 마이그레이션
     for board_type, url in issue_urls.items():
         issue_board_r = requests.request("GET", url)
         issue_board_xml = making_soup(issue_board_r.content, 'xml')
@@ -100,6 +200,7 @@ def issue_migration(**kwargs):
         artifacts = issue_board_xml.findAll(tag_name)
 
         for id_tag in tqdm(artifacts):
+            # 본문 파싱
             artifact_id = id_tag.get_text()
             request_url = '{0}/{1}/{2}.xml'.format(project.project_url, board_type, artifact_id)
             artifact_r = requests.request("GET", request_url)
@@ -113,9 +214,7 @@ def issue_migration(**kwargs):
 
             author = parsed.author.get_text().replace(' ', '')
             assignee = parsed.assignee.get_text().replace(' ', '')
-
             title = parsed.title.get_text()
-
             body = parsed.description.get_text()
             open_date_str = parsed.open_date.get_text()
 
@@ -125,45 +224,17 @@ def issue_migration(**kwargs):
             close_date = parsed.find('close_date').get_text()
             closed = False if close_date == '0' else True
 
-            attachments = parsed.attachments
-            attach_links = ''
+            # 첨부파일 파싱
             attach_download_path = os.path.join(WIKI_DIR_NAME, project_name, 'attachFile')
 
-            if attachments:
-                attach_links = '\n\n-----\n### Attachments\n'
+            attachments = parsed.attachments
+            attach_markdown = '\n\n-----\n### Attachments\n'
 
-                for item in attachments:
-                    body_file_link = item.find('link').get_text()
-                    body_file_name = item.find('name').get_text()
-                    body_file_id = item.find('id').get_text()
+            attach_links = '' if not attachments else get_attach_links(attachments, attach_download_path,
+                                                                       project.api_url, github_username, repo.name,
+                                                                       artifact_id, github_url)
 
-                    down_url = "{0}{1}".format(project.api_url, body_file_link)
-                    file_download(
-                        attach_download_path,
-                        down_url,
-                        body_file_name,
-                        artifact_id,
-                        body_file_id
-                    )
-
-                    github_parse_url = urlparse(gh._github_url)
-
-                    github_url = 'github.com' if github_parse_url.netloc == 'api.github.com' \
-                        else github_parse_url.netloc
-
-                    body_uploaded_link = 'https://{5}/{0}/{1}/wiki/attachFile/{2}/{3}/{4}'.format(
-                        github_username,
-                        repo.name,
-                        artifact_id,
-                        body_file_id,
-                        body_file_name,
-                        github_url)
-
-                    attach_links += '* {0}\n\n\t![{0}]({1})\n\n'.format(body_file_name, body_uploaded_link)
-
-                # 본문 첨부파일 없으면 Attachments 출력 x
-                if attach_links is '\n\n-----\n### Attachments\n':
-                    attach_links = ''
+            attach_markdown = '' if not attach_links else attach_markdown + attach_links
 
             assignee_text = 'and assigned to **{0}**'.format(assignee) if assignee is not 'Nobody' else ''
             description = "This {0} created by **{1}** {2} | {3}\n\n------\n\n{4}{5}".format(
@@ -172,68 +243,12 @@ def issue_migration(**kwargs):
                 assignee_text,
                 open_date,
                 body,
-                attach_links)
+                attach_markdown)
 
-            comments = parsed.comments
-            processed_comment_list = []
-
-            if comments:
-                comment_list = comments.find_all('item')
-                comment_attach_links = ''
-
-                for comment in comment_list:
-                    comment_attachments = comment.attachments
-
-                    if comment.find('pubDate') is None \
-                            or comment.find('description') is None \
-                            or comment.find('author') is None \
-                            or comment.find('id') is None:
-                        continue
-
-                    comment_id = comment.find('id').get_text()
-                    comment_body = comment.find('description').get_text()
-                    comment_author = comment.find('author').get_text()
-                    comment_date = comment.find('pubDate').get_text()
-
-                    print_date_type = "%Y/%m/%d %H:%M:%S"
-                    comment_time_type = "%Y-%m-%dT%H:%M:%SZ"
-
-                    print_comment_time = time.strftime(print_date_type,
-                                                       time.localtime(int(comment_date)))
-                    comment_time = time.strftime(comment_time_type,
-                                                 time.localtime(int(comment_date)))
-
-                    if comment_attachments:
-                        comment_attach_links = '\n\n-----\n### Attachments\n'
-                        comment_items = comment_attachments.find_all('item')
-
-                        for item in comment_items:
-                            comment_file_id = item.find('id').get_text()
-                            file_link = item.find('link').get_text()
-                            file_name = file_link.split('/')[-1]
-                            down_url = "{0}{1}".format(project.api_url, file_link)
-
-                            file_download(attach_download_path, down_url, file_name, artifact_id, comment_file_id,
-                                          comment_id=comment_id)
-
-                            comment_uploaded_link = 'https://github.com/{0}/{1}/wiki/attachFile/{2}/{3}/{4}/{5}'.format(
-                                github_username,
-                                repo.name,
-                                artifact_id,
-                                comment_id,
-                                comment_file_id,
-                                file_name)
-                            comment_attach_links += '* {0}\n\n\t![{0}]({1})\n\n'.format(file_name,
-                                                                                        comment_uploaded_link)
-
-                    c_body = "This comment created by **{0}** | {1}\n\n------\n\n{2}{3}".format(
-                        comment_author, print_comment_time, comment_body, comment_attach_links
-                    )
-
-                    processed_comment_list.append({
-                        "created_at": comment_time,
-                        "body": c_body
-                    })
+            # 코멘트 파싱
+            comment_list = [] if not parsed.comments else get_comments(parsed.comments, project.api_url,
+                                                                       attach_download_path, artifact_id,
+                                                                       github_username, repo.name)
 
             issue_json = json.dumps(
                 dict(
@@ -241,9 +256,9 @@ def issue_migration(**kwargs):
                         title=title,
                         body=description,
                         closed=closed,
-                        labels=[board_type, 'OPEN PROJECT']
+                        labels=[board_type]
                     ),
-                    comments=processed_comment_list
+                    comments=comment_list
                 )
             )
 
--- migration/milestone.py
+++ migration/milestone.py
@@ -6,28 +6,16 @@ import time
 
 
 class Milestone:
-    """
-     <milestone>
-        <id>676</id>
-        <group_artifact_id>143</group_artifact_id>
-        <element_id>71587</element_id>
-        <features>미뤄둘 일들</features>
-        <duedate>1609426799</duedate>
-        <status>PROGRESS</status>
-    </milestone>
-    """
-
     def __init__(self, milestone):
         self.id = milestone.id.get_text()
         self.group_artifact_id = milestone.group_artifact_id.get_text()
-        self.features = milestone.element_id.get_text()
+        self.features = milestone.features.get_text()
         self.due_date = milestone.duedate.get_text()
         self.status = milestone.status.get_text()
 
-    def __str__(self):
         status = 'open' if self.status is 'PROGRESS' else 'closed'
 
-        return json.dumps(
+        self.json = json.dumps(
             dict(
                 title=self.features,
                 state=status,
@@ -35,3 +23,6 @@ class Milestone:
                 due_on=time.strftime("%Y-%m-%dT%H:%M:%SZ", time.localtime(int(self.due_date)))
             )
         )
+
+    def __str__(self):
+        return self.json
